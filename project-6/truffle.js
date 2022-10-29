519dde1a365f888ab2040506737e38154de6bad2
Aldo Gonzalez <aldo.linux@gmail.com>
2022-10-29 08:54:57 -0400

    Fixed some issues while testing in Ganache UI

--- project-6/truffle.js
+++ project-6/truffle.js
@@ -2,7 +2,7 @@ module.exports = {
   networks: {
     development: {
       host: "127.0.0.1",
-      port: 9545,
+      port: 7545, // Changed this to test agains Ganache UI instead of ganache-cli
       network_id: "*" // Match any network id
     }
   }
