4fbbd2d0e3789974505179f3bed6ac3f7bce7f30
Aldo Gonzalez <aldo.linux@gmail.com>
2022-11-11 12:29:11 -0500

    Tested in Goerli ready for submission

--- project-6/src/js/app.js
+++ project-6/src/js/app.js
@@ -250,7 +250,7 @@ App = {
         var processId = parseInt($(event.target).data('id'));
 
         App.contracts.SupplyChain.deployed().then(function(instance) {
-            const walletValue = web3.toWei(3, "ether");
+            const walletValue = web3.toWei(0.000000001, "ether");
             return instance.buyItem(App.upc, {from: App.metamaskAccountID, value: walletValue});
         }).then(function(result) {
             $("#ftc-item").text(result);
--- project-6/truffle.js
+++ project-6/truffle.js
@@ -1,9 +1,26 @@
+const HDWalletProvider = require("truffle-hdwallet-provider");
+const infuraKey = "e9c44a4298a9463bb0cc8876220d8af7";
+
+const fs = require('fs');
+const mnemonic = fs.readFileSync(".secret").toString().trim();
+
 module.exports = {
   networks: {
     development: {
       host: "127.0.0.1",
-      port: 7545, // Changed this to test agains Ganache UI instead of ganache-cli
+      port: 7545,
       network_id: "*" // Match any network id
+    },
+    goerli: {
+      provider: () => new HDWalletProvider(mnemonic, `https://goerli.infura.io/v3/${infuraKey}`),
+      network_id: 5,       // goerli's id 
+      gas: 4500000,        // goerli has a lower block limit than mainnet
+      gasPrice: 10000000000
+    }
+  },
+  compilers: {
+    solc: {
+      version: "0.4.24"
     }
   }
 };
